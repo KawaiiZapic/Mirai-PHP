8966abbc3eb475949b3325565ba01f36bfb6277c
Zapic <zapic233@qq.com>
2020-07-15 14:17:25 +0800

    Fixed upload image&some doc

--- src/Bot.php
+++ src/Bot.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * PHP SDK for Mirai HTTP API
+ * 
+ * @author Zapic <kawaiizapic@zapic.cc>
+ * @license AGPL-3.0
+ * @version beta-0.0.1
+ * 
+ * @package Mirai
+ * 
+ */
+
 namespace Mirai;
 
 require_once "Events.php";
@@ -6,10 +17,25 @@ require_once "Exceptions.php";
 require_once "Messages.php";
 require_once "Objects.php";
 
+/**
+ * Bot Instance
+ */
+
 class Bot {
     private $_authKey;
     private $_session;
     private $_conn;
+    private $_qq;
+
+    /**
+     * Connect to HTTP API
+     * 
+     * @param \Swoole\Coroutine\Http\Client $client An Swoole\Coroutine\Http\Client that connected to HTTP API
+     * @param string $authKey Authkey for HTTP API
+     * @param int $target QQ number of the target bot
+     * 
+     * @return void
+     */
 
     public function __construct(\Swoole\Coroutine\Http\Client $client, string $authKey,int $target) {
         $this->_conn = $client;
@@ -18,8 +44,24 @@ class Bot {
         $this->login();
     }
 
+    /**
+     * Login to HTTP API
+     * 
+     * @throws InvaildKeyException Invaild Authkey
+     * @throws InvaildRespondException Server may not be a Mirai HTTP API
+     * @throws ConnectFaliedError Failed to connect to HTTP API
+     * @throws BindFailedException API return an error code while try to bind session to QQ
+     * 
+     * @return void
+     */
+
     private function login():void{
         $client = &$this->_conn;
+        $client->get("/about");
+        $info = json_decode($client->body);
+        if(!$info || $info->code != 0){
+            throw new \Exception("Invaild respond,it doesn't seem like a vaild HTTP API.");
+        }
         $ret = $client->post("/auth", json_encode(["authKey" => $this->_authKey]));
         if ($ret) {
             $ret = json_decode($client->body);
@@ -43,22 +85,57 @@ class Bot {
         $this->callBotAPI("/config",["enableWebsocket"=>true]);
     }
 
-    public function setFetchCallback(callable $callback){
+    /**
+     * Set callback function to handle bot events.
+     * Callback function will recive 2 params,first is decoded json,last is the raw content.
+     * 
+     * @param callable $callback Function that handle callback
+     * 
+     * @throws ConnectionCloseError Connection to API closed.
+     * @throws UpgradeFailedError Failed to upgrade connection to Websocket.
+     * @throws Error Some unknown error occured 
+     * 
+     * @return void
+     * 
+     */
+
+    public function setEventHandler(callable $callback):void{
         $this->_conn = new \Swoole\Coroutine\Http\Client($this->_conn->host, $this->_conn->port,$this->_conn->ssl);
         $ret = $this->_conn->upgrade("/all?sessionKey={$this->_session}");
+        if(!$ret || $this->_conn->statusCode != 101){
+            throw new \Exception("Failed to upgrade connection to Websocket.");
+        }
         \go(function() use ($callback){
             while(true){
                 $frame = $this->_conn->recv();
                 if ($frame) {
                     $result = $this->EventFactory(json_decode($frame->data));
-                } elseif($frame === false && $this->_ws->errCode === 0) {
-                    throw new \Exception("Connection to Mirai HTTP API Closed.");
+                } elseif($frame === false && $this->_conn->errCode === 0) {
+                    throw new \Exception("Connection to Mirai HTTP API Closed:{$this->_conn->errMsg}.");
+                } elseif($this->_conn->errCode == 104 && $this->_conn->closeNormal === true) {
+                    break;
+                } else {
+                    throw new \Exception("Unknown error while recv from Websocket.");
                 }
-                call_user_func_array($callback,[$result]);
+                call_user_func_array($callback,[$result,$frame->data]);
             }
         });
     }
-    public function callBotAPI(string $path, array $params = [], string $method = "post"){
+    /**
+     * Send request to API
+     * 
+     * @param string $path API to request
+     * @param array $params Params that pass to API
+     * @param string $method Default "post",Request method,only "post" or "get"
+     * @param bool $raw Default false,if true,will return the raw content but not the decoded content.
+     * 
+     * @throws InvaildParamException Request method may not correct
+     * @throws FetchFailedException Failed to fecth response form API.
+     * 
+     * @return any API respose
+     */
+
+    public function callBotAPI(string $path, array $params = [], string $method = "post",$raw = false){
         $client = new \Swoole\Coroutine\Http\Client($this->_conn->host, $this->_conn->port,$this->_conn->ssl);
         if ($method == "post") {
             $params['sessionKey'] = $this->_session;
@@ -78,25 +155,62 @@ class Bot {
         }
         $client->close();
         if ($ret) {
-            return json_decode($client->body);
+            return $raw === true ? $client->body : json_decode($client->body);
         } else {
             throw new \Exception("Failed to fetch API response");
         }
     }
 
+    /**
+     * Get QQ ID of this Bot instance
+     * 
+     * @return int QQ ID
+     * 
+     */
+
     public function getId():int{
         return $this->_qq;
     }
 
-    public function ShutDown(){
+    /**
+     * Close connection to HTTP API
+     * 
+     * @return void
+     * 
+     */
+
+    public function ShutDown():void{
+        $this->_conn->closeNormal = true;
         $this->_conn->close();
     }
+
+    /**
+     * Upload image to Tecent server
+     * 
+     * @param string $type Type of image,"friend" or "group".
+     * @param string $file Path to image.
+     * 
+     * @throws FileNotFoundException Image file not found
+     * 
+     * @return mixed API Respond.
+     */
+    
     public function uploadImage(string $type, string $file) {
-        $client = new \Swoole\Coroutine\Http\Client($this->_conn->host, $this->_conn->port,$this->_conn->ssl);
-        $client->addFile($file, "img");
-        $client->addData($this->_session, "sessionKey");
-        $client->addData($type, "type");
-        $client->post("/uploadImage");
+        if(!file_exists($file) || !is_file($file)){
+            throw new \Exception("File not found.");
+        }
+	    $boundary = uniqid();
+	    $client = new \Swoole\Coroutine\Http\Client($this->_conn->host, $this->_conn->port,$this->_conn->ssl);
+	    $client->setHeaders([
+	        "Content-Type" => "multipart/form-data; boundary={$boundary};",
+        ]);
+	    $client->setMethod("POST");
+	    $body = "--{$boundary}\r\nContent-Disposition: form-data; name=\"sessionKey\"\r\n\r\n{$this->_session}\r\n";
+	    $body .= "--{$boundary}\r\nContent-Disposition: form-data; name=\"type\"\r\n\r\n{$type}\r\n";
+	    $body .= "--{$boundary}\r\nContent-Disposition: form-data; name=\"img\"; filename=\"img\"\r\nContent-Type: " . getimagesize($file)['mime'] . "\r\n\r\n" . \Co::readFile($file) . "\r\n";
+	    $body .= "--{$boundary}--\r\n";
+	    $client->setData($body);
+	    $client->execute("/uploadImage");
         $client->close();
         return json_decode($client->body);
     }
