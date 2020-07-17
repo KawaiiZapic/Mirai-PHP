fd1164c905fe0b8481a185cd151fb070e81ec553
Zapic <zapic233@qq.com>
2020-07-17 23:24:54 +0800

    Part done

--- src/Bot.php
+++ src/Bot.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * PHP SDK for Mirai HTTP API
- * 
+ *
  * @author Zapic <kawaiizapic@zapic.cc>
  * @license AGPL-3.0
  * @version beta-0.0.1
- * 
+ *
  * @package Mirai
- * 
+ *
  */
 
 namespace Mirai;
@@ -29,15 +29,15 @@ class Bot {
 
     /**
      * Connect to HTTP API
-     * 
+     *
      * @param \Swoole\Coroutine\Http\Client $client An Swoole\Coroutine\Http\Client that connected to HTTP API
      * @param string $authKey Authkey for HTTP API
      * @param int $target QQ number of the target bot
-     * 
+     *
      * @return void
      */
 
-    public function __construct(\Swoole\Coroutine\Http\Client $client, string $authKey,int $target) {
+    public function __construct(\Swoole\Coroutine\Http\Client $client, string $authKey, int $target) {
         $this->_conn = $client;
         $this->_authKey = $authKey;
         $this->_qq = $target;
@@ -46,97 +46,97 @@ class Bot {
 
     /**
      * Login to HTTP API
-     * 
+     *
      * @throws InvaildKeyException Invaild Authkey
      * @throws InvaildRespondException Server may not be a Mirai HTTP API
      * @throws ConnectFaliedError Failed to connect to HTTP API
      * @throws BindFailedException API return an error code while try to bind session to QQ
-     * 
+     *
      * @return void
      */
 
-    private function login():void{
+    private function login(): void {
         $client = &$this->_conn;
         $client->get("/about");
         $info = json_decode($client->body);
-        if(!$info || $info->code != 0){
-            throw new \Exception("Invaild respond,it doesn't seem like a vaild HTTP API.");
+        if (!$info || $info->code != 0) {
+            throw new InvaildRespondException("Invaild respond,it doesn't seem like a vaild HTTP API.");
         }
         $ret = $client->post("/auth", json_encode(["authKey" => $this->_authKey]));
         if ($ret) {
             $ret = json_decode($client->body);
             if ($ret->code != 0) {
-                throw new \Exception("Invaild AuthKey.");
+                throw new InvaildKeyException("Invaild AuthKey.");
             } else {
                 $this->_session = $ret->session;
             }
         } else {
-            throw new \Exception("Failed to connect to HTTP API.");
+            throw new ConnectFaliedError("Failed to connect to HTTP API.");
         }
         $ret = $client->post("/verify", json_encode(["sessionKey" => $this->_session, "qq" => $this->_qq]));
         if ($ret) {
             $ret = json_decode($client->body);
             if ($ret->code != 0) {
-                throw new \Exception("Failed to bind seesion to QQ({$this->_qq}):{$ret->msg}.");
+                throw new BindFailedException("Failed to bind seesion to QQ({$this->_qq}):{$ret->msg}.");
             }
         } else {
-            throw new \Exception("Failed to connect to HTTP API.");
+            throw new ConnectFaliedError("Failed to connect to HTTP API.");
         }
-        $this->callBotAPI("/config",["enableWebsocket"=>true]);
+        $this->callBotAPI("/config", ["enableWebsocket" => true]);
     }
 
     /**
      * Set callback function to handle bot events.
      * Callback function will recive 2 params,first is decoded json,last is the raw content.
-     * 
+     *
      * @param callable $callback Function that handle callback
-     * 
+     *
      * @throws ConnectionCloseError Connection to API closed.
      * @throws UpgradeFailedError Failed to upgrade connection to Websocket.
-     * @throws Error Some unknown error occured 
-     * 
+     * @throws Error Some unknown error occured.
+     *
      * @return void
-     * 
+     *
      */
 
-    public function setEventHandler(callable $callback):void{
-        $this->_conn = new \Swoole\Coroutine\Http\Client($this->_conn->host, $this->_conn->port,$this->_conn->ssl);
+    public function setEventHandler(callable $callback): void {
+        $this->_conn = new \Swoole\Coroutine\Http\Client($this->_conn->host, $this->_conn->port, $this->_conn->ssl);
         $ret = $this->_conn->upgrade("/all?sessionKey={$this->_session}");
-        if(!$ret || $this->_conn->statusCode != 101){
-            throw new \Exception("Failed to upgrade connection to Websocket.");
+        if (!$ret || $this->_conn->statusCode != 101) {
+            throw new UpgradeFailedError("Failed to upgrade connection to Websocket.");
         }
-        \go(function() use ($callback){
-            while(true){
+        \go(function () use ($callback) {
+            while (true) {
                 $frame = $this->_conn->recv();
                 if ($frame) {
                     $result = $this->EventFactory(json_decode($frame->data));
-                } elseif($frame === false && $this->_conn->errCode === 0) {
-                    throw new \Exception("Connection to Mirai HTTP API Closed:{$this->_conn->errMsg}.");
-                } elseif($this->_conn->errCode == 104 && $this->_conn->closeNormal === true) {
+                } elseif ($frame === false && $this->_conn->errCode === 0) {
+                    throw new ConnectionCloseError("Connection to Mirai HTTP API Closed:{$this->_conn->errMsg}.");
+                } elseif ($this->_conn->errCode == 104 && $this->_conn->closeNormal === true) {
                     break;
                 } else {
-                    throw new \Exception("Unknown error while recv from Websocket.");
+                    throw new \Error("Unknown error while recv from Websocket.");
                 }
-                call_user_func_array($callback,[$result,$frame->data]);
+                call_user_func_array($callback, [$result, $frame->data]);
             }
         });
     }
     /**
      * Send request to API
-     * 
+     *
      * @param string $path API to request
      * @param array $params Params that pass to API
      * @param string $method Default "post",Request method,only "post" or "get"
      * @param bool $raw Default false,if true,will return the raw content but not the decoded content.
-     * 
-     * @throws InvaildParamException Request method may not correct
-     * @throws FetchFailedException Failed to fecth response form API.
-     * 
-     * @return any API respose
+     *
+     * @throws IllegalParamException Request method may not correct
+     * @throws FetchFailedError Failed to fecth response form API.
+     *
+     * @return any API response.
      */
 
-    public function callBotAPI(string $path, array $params = [], string $method = "post",$raw = false){
-        $client = new \Swoole\Coroutine\Http\Client($this->_conn->host, $this->_conn->port,$this->_conn->ssl);
+    public function callBotAPI(string $path, array $params = [], string $method = "post", $raw = false) {
+        $client = new \Swoole\Coroutine\Http\Client($this->_conn->host, $this->_conn->port, $this->_conn->ssl);
         if ($method == "post") {
             $params['sessionKey'] = $this->_session;
             $params = json_encode($params);
@@ -151,70 +151,83 @@ class Bot {
             $ret = $client->get("{$path}?sessionKey={$this->_session}{$qstr}");
         } else {
             $client->close();
-            throw new \Exception("Unknown request method {$method}");
+            throw new IllegalParamsException("Unknown request method {$method}");
         }
         $client->close();
         if ($ret) {
             return $raw === true ? $client->body : json_decode($client->body);
         } else {
-            throw new \Exception("Failed to fetch API response");
+            throw new FetchFailedError("Failed to fetch API response");
         }
     }
 
     /**
      * Get QQ ID of this Bot instance
-     * 
+     *
      * @return int QQ ID
-     * 
+     *
      */
 
-    public function getId():int{
+    public function getId(): int {
         return $this->_qq;
     }
 
     /**
      * Close connection to HTTP API
-     * 
+     *
      * @return void
-     * 
+     *
      */
 
-    public function ShutDown():void{
+    public function ShutDown(): void {
         $this->_conn->closeNormal = true;
         $this->_conn->close();
     }
 
     /**
      * Upload image to Tecent server
-     * 
+     *
      * @param string $type Type of image,"friend" or "group".
      * @param string $file Path to image.
-     * 
+     *
      * @throws FileNotFoundException Image file not found
-     * 
-     * @return mixed API Respond.
+     *
+     * @return mixed API Response.
      */
-    
+
     public function uploadImage(string $type, string $file) {
-        if(!file_exists($file) || !is_file($file)){
-            throw new \Exception("File not found.");
+        if (!file_exists($file) || !is_file($file)) {
+            throw new FileNotFoundException("File not found.");
         }
-	    $boundary = uniqid();
-	    $client = new \Swoole\Coroutine\Http\Client($this->_conn->host, $this->_conn->port,$this->_conn->ssl);
-	    $client->setHeaders([
-	        "Content-Type" => "multipart/form-data; boundary={$boundary};",
+        $boundary = "----MiraiBoundary" . uniqid();
+        $client = new \Swoole\Coroutine\Http\Client($this->_conn->host, $this->_conn->port, $this->_conn->ssl);
+        $client->setHeaders([
+            "Content-Type" => "multipart/form-data; boundary={$boundary};",
         ]);
-	    $client->setMethod("POST");
-	    $body = "--{$boundary}\r\nContent-Disposition: form-data; name=\"sessionKey\"\r\n\r\n{$this->_session}\r\n";
-	    $body .= "--{$boundary}\r\nContent-Disposition: form-data; name=\"type\"\r\n\r\n{$type}\r\n";
-	    $body .= "--{$boundary}\r\nContent-Disposition: form-data; name=\"img\"; filename=\"img\"\r\nContent-Type: " . getimagesize($file)['mime'] . "\r\n\r\n" . \Co::readFile($file) . "\r\n";
-	    $body .= "--{$boundary}--\r\n";
-	    $client->setData($body);
-	    $client->execute("/uploadImage");
+        $client->setMethod("POST");
+        $body = "--{$boundary}\r\nContent-Disposition: form-data; name=\"sessionKey\"\r\n\r\n{$this->_session}\r\n";
+        $body .= "--{$boundary}\r\nContent-Disposition: form-data; name=\"type\"\r\n\r\n{$type}\r\n";
+        $body .= "--{$boundary}\r\nContent-Disposition: form-data; name=\"img\"; filename=\"img\"\r\nContent-Type: " . getimagesize($file)['mime'] . "\r\n\r\n" . \Co::readFile($file) . "\r\n";
+        $body .= "--{$boundary}--\r\n";
+        $client->setData($body);
+        $client->execute("/uploadImage");
         $client->close();
         return json_decode($client->body);
     }
 
+    /**
+     * Send image to target by URL
+     * If $qq is not null,image will send to private chat.
+     * If $gorup is not null,image will send to group chat.
+     * If both $group and $target is not null,image will send to temp chat.
+     *
+     * @param string $urls Url of image
+     * @param int $qq Target friend user.
+     * @param int $group Target group.
+     * @param int $target Target group user.
+     *
+     * @return mixed API Response.
+     */
     public function sendImageMessage(string $urls, int $qq = null, int $group = null, int $target = null) {
         $pre = [];
         if (!is_null($qq)) {
@@ -230,7 +243,18 @@ class Bot {
         return $this->callBotAPI("/sendImageMessage", $pre);
     }
 
-    public function sendFriendMessage(int $target, array $chain, bool $quote = null) {
+    /**
+     * Send messgae to friend
+     *
+     * @param int $target Target user to recive messgae.
+     * @param array $chain Message chain to send.
+     * @param int $quote Specify a Message Id to quote.
+     *
+     * @return mixed API Response.
+     *
+     */
+
+    public function sendFriendMessage(int $target, array $chain, int $quote = null) {
         $pre = [
             "target" => $target,
             "messageChain" => $chain,
@@ -241,7 +265,19 @@ class Bot {
         return $this->callBotAPI("/sendFriendMessage", $pre);
     }
 
-    public function sendTempMessage(int $qq, int $group, array $chain, bool $quote = null) {
+    /**
+     * Send message to temp user
+     *
+     * @param int $qq Target user
+     * @param int $group Chat source group
+     * @param array $chain Message chain to send
+     * @param int quote Specify a Message Id to quote
+     *
+     * @return mixed API response.
+     *
+     */
+
+    public function sendTempMessage(int $qq, int $group, array $chain, int $quote = null) {
         $pre = [
             "qq" => $qq,
             "group" => $group,
@@ -252,7 +288,19 @@ class Bot {
         }
         return $this->callBotAPI("/sendTempMessage", $pre);
     }
-    public function sendGroupMessage(int $target, array $chain, bool $quote = null) {
+
+    /**
+     *
+     * Send message to group
+     *
+     * @param int $target Target group
+     * @param array $chain Message chain to send
+     * @param int $quote Specify a Message Id to quote
+     *
+     * @return mixed API Response
+     *
+     */
+    public function sendGroupMessage(int $target, array $chain, int $quote = null) {
         $pre = [
             "target" => $target,
             "messageChain" => $chain,
@@ -263,41 +311,158 @@ class Bot {
         return $this->callBotAPI("/sendGroupMessage", $pre);
     }
 
+    /**
+     *
+     * Recall a message
+     *
+     * @param int $id ID of message which need recall
+     *
+     * @return mixed API response
+     *
+     */
     public function recallMessage(int $id) {
         return $this->callBotAPI("/recall", ["target" => $id]);
     }
+
+    /**
+     *
+     * Get qq friend list
+     *
+     * @return mixed API response
+     *
+     */
 
     public function getFriendList() {
         return $this->callBotAPI("/friendList", [], "get");
     }
 
+    /**
+     * Get qq group list
+     *
+     * @return mixed API response
+     *
+     */
+
     public function getGroupList() {
         return $this->callBotAPI("/groupList", [], "get");
     }
+
+    /**
+     *
+     * Get group member list
+     *
+     * @param int $target Target group
+     *
+     * @return mixed API response
+     *
+     */
 
     public function getMemberList(int $target) {
         return $this->callBotAPI("/groupList", ["target" => $target], "get");
     }
 
+    /**
+     * Mute hole group
+     *
+     * @param int $target Target group
+     *
+     * @return mixed API response
+     *
+     */
+
     public function muteAll(int $target) {
         return $this->callBotAPI("/muteAll", ["target" => $target]);
     }
+
+    /**
+     * Unmute hole group
+     *
+     * @param int $target Target group
+     *
+     * @return mixed API response
+     */
+
     public function unmuteAll(int $target) {
         return $this->callBotAPI("/unmuteAll", ["target" => $target]);
     }
+
+    /**
+     *
+     * Mute a group member
+     *
+     * @param int $group Group which target in
+     * @param int $qq Target member
+     * @param int $time Mute time,default 0
+     *
+     * @return mixed API response
+     *
+     */
+
     public function muteMember(int $group, int $qq, int $time = 0) {
         return $this->callBotAPI("/mute", ["target" => $group, "memberId" => $qq, "time" => $time]);
     }
+
+    /**
+     *
+     * Unmute a group member
+     *
+     * @param int $group Group which target in
+     * @param int $target Target member
+     *
+     * @return mixed API response
+     *
+     */
+
     public function unmuteMember(int $group, int $qq) {
         return $this->callBotAPI("/unmute", ["target" => $group, "memberId" => $qq]);
     }
+
+    /**
+     *
+     * Kick a member from group
+     *
+     * @param int $group Group which target in
+     * @param int $qq Target member
+     * @param string $msg Kick message
+     *
+     * @return mixed API response
+     *
+     */
+
     public function kickMember(int $group, int $qq, string $msg = "") {
         return $this->callBotAPI("/kick", ["target" => $group, "memberId" => $qq, "msg" => $msg]);
     }
+
+    /**
+     *
+     * Leave a group
+     *
+     * @param int $target Group to leave
+     *
+     * @return mixed API Response
+     *
+     */
+
     public function quitGroup(int $target) {
         return $this->callBotAPI("/quit", ["target" => $target]);
     }
-    public function GroupConfig(int $target, string $name, string $value = null) {
+
+    /**
+     * 
+     * Get/Set config of group
+     * This is a jQuery-like function
+     * If the thrid param is null,function will return the config vale
+     * If the thrid param is not null,the value will be save to group
+     * 
+     * @param int $target Target group
+     * @param string $name Config name
+     * @param mixed $value Value to set
+     * 
+     * @return mixed API response
+     * 
+     */
+
+    public function GroupConfig(int $target, string $name, $value = null) {
         $d = $this->_bot->callBotAPI("/groupConfig", ["target" => $target], "get");
         if ($name == null) {
             return $d;
@@ -308,6 +473,21 @@ class Bot {
         }
         return $d->$name;
     }
+    /**
+     * 
+     * Get/Set info of group member
+     * This is a jQuery-like function
+     * If the thrid param is null,function will return the info vale
+     * If the thrid param is not null,the value will be save to member info
+     * 
+     * @param int $target Group which target member in
+     * @param int $qq Target member
+     * @param string $name Config name
+     * @param mixed $value Value to set
+     * 
+     * @return mixed API response
+     * 
+     */
 
     public function memberInfo(int $target, int $qq, string $name = null, $value = null) {
         $d = $this->_bot->callBotAPI("/memberInfo", ["target" => $target, "memberId" => $qq], "get");
@@ -320,6 +500,10 @@ class Bot {
         }
         return $d->$name;
     }
+
+    /**
+     * Register a command 
+     */
 
     public function registerCommand(string $name, array $alias = [], string $desc = "", string $usage = null) {
         return $this->callBotAPI("/command/register", [
@@ -341,12 +525,12 @@ class Bot {
         return $this->callBotAPI("managers?qq={$this->_qq}");
     }
 
-    public function EventFactory($frame){
-        $msgEvt = ["GroupMessage","FriendMessage","TempMessage"];
-        if(in_array($frame->type,$msgEvt)){
+    public function EventFactory($frame) {
+        $msgEvt = ["GroupMessage", "FriendMessage", "TempMessage"];
+        if (in_array($frame->type, $msgEvt)) {
             $frame->type .= "Event";
         }
-        $evt = "\\Mirai\\".$frame->type;
-        return new $evt($frame,$this);
+        $evt = "\\Mirai\\" . $frame->type;
+        return new $evt($frame, $this);
     }
 }
--- src/Exceptions.php
+++ src/Exceptions.php
@@ -2,3 +2,14 @@
 namespace Mirai;
 class IllegalParamsException extends \Exception{}
 class IllegalMessageObjectException extends \Exception{}
+
+class InvaildKeyException extends \Exception{}
+class InvaildRespondException extends \Exception{}
+class FileNotFoundException extends \Exception{}
+class BindFailedException extends \Exception{}
+
+class ConnectionError extends \Error{};
+class ConnectionCloseError extends ConnectionError{}
+class ConnectFaliedError extends ConnectionError{}
+class UpgradeFailedError extends ConnectionError{}
+class FetchFailedError extends \Error{}
